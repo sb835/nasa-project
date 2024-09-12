79388f998893b8062a5942fdb39926a187074886
sb835 <silas-backhaus@web.de>
2024-09-12 13:03:46 +0200

    a

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -4,8 +4,6 @@ const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
 const { loadPlanetsData } = require('../../models/planets.model');
 
-const { loadPlanetsData } = require('../../models/planets.model');
-
 describe('Launches API', () => {
     beforeAll(async () => {
         await mongoConnect();
